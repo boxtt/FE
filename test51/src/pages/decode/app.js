317a67be7d012509c37fb12249e56e5889243272
Wk.Zhao <wkzhao@BJ2132.local>
2018-05-07 14:57:09 +0800

    add audiorecoder

---
+++ test51/src/pages/decode/app.js
@@ -0,0 +1,75 @@
+import shell from './shell.js'; // 固定，引入shell
+import {
+    App,
+} from '@fes/mkv-frame'; // 固定
+import mm from '@fes/mo-bridge';
+import mk from '@fes/mo-mock';
+import mma from '@fes/mo-mma';
+import './app.scss';
+
+new App({
+    shell,
+    components: {},
+    created() {
+        this.init();
+        // 发送请求
+    },
+    methods: {
+        init() {
+            let _this = this;
+            this.audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+            this.offlineCtx = new (window.OfflineAudioContext || window.webkitOfflineAudioContext)(2, 44100 * 120, 44100);
+            this.source = this.offlineCtx.createBufferSource();
+            this.getData();
+        },
+        ready() {
+            // this.isReady = true;
+            console.log('media ready!');
+            this.sourceMedia = this.audioCtxMedia.createMediaElementSource(this.myMedia);
+            this.sourceMedia.connect(this.analyserMedia);
+            this.analyserMedia.connect(this.audioCtxMedia.destination);
+            this.currentSourceElement.play();
+        },
+
+        btnClick() {
+            this.song.start(0,5,10);
+        },
+
+        getData() {
+            var _this=this;
+            var request = new XMLHttpRequest();
+
+            request.open('GET', '220788b1285c6b542a9941cc0d16936fef311528e3f0000ac50542caf77.mp4', true);
+
+            request.responseType = 'arraybuffer';
+
+            request.onload = function () {
+                var audioData = request.response;
+
+                _this.audioCtx.decodeAudioData(audioData, function (buffer) {
+                    _this.source.buffer = buffer;
+                    _this.source.connect(_this.offlineCtx.destination);
+                    _this.source.start();
+                    //source.loop = true;
+                    _this.offlineCtx.startRendering().then(function (renderedBuffer) {
+                        console.log('渲染完全成功');
+                        _this.audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+                        _this.song = _this.audioCtx.createBufferSource();
+                        _this.song.buffer = renderedBuffer;
+
+                        _this.song.connect(_this.audioCtx.destination);
+
+
+                    }).catch(function (err) {
+                        console.log('渲染失败: ' + err);
+                        // 注意: 当 OfflineAudioContext 上 startRendering 被立刻调用，Promise 应该被 reject
+                    });
+                });
+            }
+
+            request.send();
+        }
+
+    },
+
+});
---
+++ test51/src/pages/decode/shell.js
@@ -0,0 +1,34 @@
+import Vue from 'vue';
+import {
+    Shell,
+} from '@fes/mkv-frame'; // 固定
+import Demo from 'components/demo';
+import '@fes/mo-static/css/ui.reset.css'; // 可引入static静态资源
+import './shell.scss';
+
+Vue.use(Shell); // 固定
+
+// html基本组件、data
+const shell = new Shell({
+    el: '#root',
+    components: {
+        Demo,
+    },
+    data: {
+        atlas: null,
+        birds: [],
+        sourceFrom: 'stream',
+        msg: '',
+        isReady: false,
+        audioCtx1: null,
+        audioCtx2: null,
+        myMedia: null,
+        myStream: null,
+        currentSourceElement: null,
+        source: null,
+        analyser: null,
+        canvas: null,
+    },
+});
+
+export default shell;
